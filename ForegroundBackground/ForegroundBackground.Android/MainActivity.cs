0af12636ac19710dc4e11b6f617a4fb36c7b86c3
Martin Finkel <martin@videolabs.io>
2018-12-15 16:08:49 +0700

    Add ForegroundBackground Xamarin.Forms sample

---
+++ ForegroundBackground/ForegroundBackground.Android/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Xamarin.Forms;
+using LibVLCSharp.Forms.Shared;
+
+namespace ForegroundBackground.Droid
+{
+    [Activity(Label = "ForegroundBackground", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            LibVLCSharpFormsRenderer.Init();
+            Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+
+            MessagingCenter.Send("app", "OnStop");
+        }
+
+        protected override void OnRestart()
+        {
+            base.OnRestart();
+
+            MessagingCenter.Send("app", "OnRestart");
+        }
+    }
+}
---
+++ ForegroundBackground/ForegroundBackground/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using LibVLCSharp.Forms.Shared;
+using LibVLCSharp.Shared;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace ForegroundBackground
+{
+    public partial class MainPage : ContentPage
+    {
+        LibVLC _libVLC;
+        MediaPlayer _mediaPlayer;
+        VideoView _videoView;
+        float _position;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MessagingCenter.Subscribe<string>(this, "OnStop", app =>
+            {
+                VideoView.Loaded -= VideoView_Loaded;
+                _position = _mediaPlayer.Position;
+                _mediaPlayer.Stop();
+                MainGrid.Children.Clear();
+                Debug.WriteLine($"saving mediaplayer position {_position}");
+            });
+
+            MessagingCenter.Subscribe<string>(this, "OnRestart", app =>
+            {
+                _videoView = new VideoView { HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.FillAndExpand };
+                MainGrid.Children.Add(_videoView);
+
+                _videoView.Loaded += VideoView_Loaded;
+
+                _videoView.MediaPlayer = _mediaPlayer;
+                _videoView.MediaPlayer.Position = _position;
+            });
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            VideoView.Loaded += VideoView_Loaded;
+
+            Core.Initialize();
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC)
+            {
+                Media = new Media(_libVLC,
+                "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4",
+                Media.FromType.FromLocation)
+            };
+
+            VideoView.MediaPlayer = _mediaPlayer;
+        }
+
+        private void VideoView_Loaded(object sender, System.EventArgs e)
+        {
+            _mediaPlayer.Play();
+        }
+    }
+}
