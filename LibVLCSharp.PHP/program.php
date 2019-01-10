c847be74c41013a942a589e75b1cdc8ef2e8a0c0
Martin Finkel <martin@videolabs.io>
2019-01-10 15:38:25 +0700

    Add PHP peachpie PoC

---
+++ LibVLCSharp.PHP/program.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+use LibVLCSharp\Shared\Core as Core;
+use LibVLCSharp\Shared\LibVLC as LibVLC;
+use LibVLCSharp\Shared\MediaPlayer as MediaPlayer;
+use LibVLCSharp\Shared\Media as Media;
+
+# Hack to compute libvlc.dll/libvlccore.dll path for PHP projects
+list($scriptPath) = get_included_files();
+$p = str_replace("program.php", "", $scriptPath);
+$libvlcPath = $p . "bin\\Debug\\netcoreapp2.0";
+
+var_dump(Core::Initialize($libvlcPath));
+
+$libVLC = new LibVLC();
+$mediaPlayer = new MediaPlayer($libVLC);
+$media = new Media($libVLC, "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4", 1);
+
+$mediaPlayer->Play($media);
+
+echo "Loading...";
+$handle = fopen ("php://stdin","r");
+$line = fgets($handle);
+if(trim($line) != 'yes'){
+    echo "ABORTING!\n";
+    exit;
+}
+fclose($handle);
