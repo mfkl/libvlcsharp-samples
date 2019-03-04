470dc5a42dcdf80278d6f4814a1f2f975caee8e8
Martin Finkel <martin@videolabs.io>
2019-03-04 09:59:09 +0700

    Add minimal playback WPF sample

---
+++ MinimalPlayback/LibVLCSharp.WPF.Sample/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace LibVLCSharp.WPF.Sample
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ MinimalPlayback/LibVLCSharp.WPF.Sample/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using LibVLCSharp.Shared;
+using System.Windows;
+
+namespace LibVLCSharp.WPF.Sample
+{
+    public partial class MainWindow : Window
+    {
+        LibVLC _libVLC;
+        MediaPlayer _mediaPlayer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            videoView.Loaded += VideoView_Loaded;
+        }
+
+        void VideoView_Loaded(object sender, RoutedEventArgs e)
+        {
+            Core.Initialize();
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            videoView.MediaPlayer = _mediaPlayer;
+
+            _mediaPlayer.Play(new Media(_libVLC, "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4", FromType.FromLocation));
+        }
+    }
+}
