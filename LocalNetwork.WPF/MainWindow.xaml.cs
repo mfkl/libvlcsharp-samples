aedb7794e1dbb87c602d1e1fae2d3d6139c7f5dd
Martin Finkel <martin@videolabs.io>
2019-04-23 10:50:11 +0700

    Add WPF back end

---
+++ LocalNetwork.WPF/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using LibVLCSharp.Forms.Platforms.WPF;
+using LibVLCSharp.Forms.Shared;
+using System.Collections.Generic;
+using System.Reflection;
+using Xamarin.Forms.Platform.WPF;
+
+namespace LocalNetwork.WPF
+{
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitDependencies();
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new LocalNetwork.App());
+        }
+
+        void InitDependencies()
+        {
+            var init = new List<Assembly>
+            {
+                typeof(VideoView).Assembly,
+                typeof(VideoViewRenderer).Assembly
+            };
+        }
+    }
+}
