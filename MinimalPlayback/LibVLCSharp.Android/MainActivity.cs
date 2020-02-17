ad7eeebb24312b3c4dbfa384b802b127efdb56c1
Martin Finkel <martin@videolabs.io>
2020-02-17 10:46:02 +0700

    Recreate Android project
    https://code.videolan.org/videolan/LibVLCSharp/issues/289

--- MinimalPlayback/LibVLCSharp.Android/MainActivity.cs
+++ MinimalPlayback/LibVLCSharp.Android/MainActivity.cs
@@ -2,13 +2,13 @@
 using Android.OS;
 using Android.Views;
 using Android.Widget;
-using VideoView = LibVLCSharp.Platforms.Android.VideoView;
+using LibVLCSharp.Platforms.Android;
 using LibVLCSharp.Shared;
-using LibVLCSharp.Android.Sample;
+using VideoView = LibVLCSharp.Platforms.Android.VideoView;
 
 namespace LibVLCSharp.Android
 {
-    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    [Activity(Label = "LibVLCSharp.Android", MainLauncher = true)]
     public class MainActivity : Activity
     {
         VideoView _videoView;
@@ -18,9 +18,9 @@ namespace LibVLCSharp.Android
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.main);
+            SetContentView(Resource.Layout.activity_main);
         }
-        
+
         protected override void OnResume()
         {
             base.OnResume();
@@ -28,7 +28,10 @@ namespace LibVLCSharp.Android
             Core.Initialize();
 
             _libVLC = new LibVLC();
-            _mediaPlayer = new MediaPlayer(_libVLC) { EnableHardwareDecoding = true };
+            _mediaPlayer = new MediaPlayer(_libVLC)
+            {
+                EnableHardwareDecoding = true
+            };
 
             _videoView = new VideoView(this) { MediaPlayer = _mediaPlayer };
             AddContentView(_videoView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent));
--- MinimalPlayback/LibVLCSharp.Android/Resources/Resource.designer.cs
+++ MinimalPlayback/LibVLCSharp.Android/Resources/Resource.designer.cs
@@ -9,9 +9,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: global::Android.Runtime.ResourceDesignerAttribute("LibVLCSharp.Android.Sample.Resource", IsApplication=true)]
+[assembly: global::Android.Runtime.ResourceDesignerAttribute("LibVLCSharp.Android.Resource", IsApplication=true)]
 
-namespace LibVLCSharp.Android.Sample
+namespace LibVLCSharp.Android
 {
 	
 	
@@ -26,6 +26,7 @@ namespace LibVLCSharp.Android.Sample
 		
 		public static void UpdateIdValues()
 		{
+			global::LibVLCSharp.Resource.String.app_name = global::LibVLCSharp.Android.Resource.String.app_name;
 		}
 		
 		public partial class Attribute
@@ -44,17 +45,17 @@ namespace LibVLCSharp.Android.Sample
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int colorAccent = 2131034114;
+			// aapt resource value: 0x7F010000
+			public const int colorAccent = 2130771968;
 			
-			// aapt resource value: 0x7f050000
-			public const int colorPrimary = 2131034112;
+			// aapt resource value: 0x7F010001
+			public const int colorPrimary = 2130771969;
 			
-			// aapt resource value: 0x7f050001
-			public const int colorPrimaryDark = 2131034113;
+			// aapt resource value: 0x7F010002
+			public const int colorPrimaryDark = 2130771970;
 			
-			// aapt resource value: 0x7f050003
-			public const int ic_launcher_background = 2131034115;
+			// aapt resource value: 0x7F010003
+			public const int ic_launcher_background = 2130771971;
 			
 			static Color()
 			{
@@ -69,8 +70,8 @@ namespace LibVLCSharp.Android.Sample
 		public partial class Dimension
 		{
 			
-			// aapt resource value: 0x7f060000
-			public const int fab_margin = 2131099648;
+			// aapt resource value: 0x7F020000
+			public const int fab_margin = 2130837504;
 			
 			static Dimension()
 			{
@@ -85,8 +86,8 @@ namespace LibVLCSharp.Android.Sample
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int main = 2130903040;
+			// aapt resource value: 0x7F030000
+			public const int activity_main = 2130903040;
 			
 			static Layout()
 			{
@@ -101,14 +102,14 @@ namespace LibVLCSharp.Android.Sample
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int ic_launcher = 2130837504;
+			// aapt resource value: 0x7F040000
+			public const int ic_launcher = 2130968576;
 			
-			// aapt resource value: 0x7f020001
-			public const int ic_launcher_foreground = 2130837505;
+			// aapt resource value: 0x7F040001
+			public const int ic_launcher_foreground = 2130968577;
 			
-			// aapt resource value: 0x7f020002
-			public const int ic_launcher_round = 2130837506;
+			// aapt resource value: 0x7F040002
+			public const int ic_launcher_round = 2130968578;
 			
 			static Mipmap()
 			{
@@ -123,11 +124,11 @@ namespace LibVLCSharp.Android.Sample
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int action_settings = 2130968577;
+			// aapt resource value: 0x7F050000
+			public const int action_settings = 2131034112;
 			
-			// aapt resource value: 0x7f040000
-			public const int app_name = 2130968576;
+			// aapt resource value: 0x7F050001
+			public const int app_name = 2131034113;
 			
 			static String()
 			{
