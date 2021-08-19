cbef314f55d67bcf8e6aee31a9bf3cc670ae70f8
Laurent Egbakou <egbakoukodjolaurent@gmail.com>
2021-08-20 00:17:58 +0200

    MediaElement Customization Sample added

---
+++ MediaElement/MediaElementCustomization/MediaElementCustomization/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.ComponentModel;
+
+namespace MediaElementCustomization
+{
+    /// <summary>
+    /// Represents the main viewmodel.
+    /// </summary>
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Property changed event
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="MainViewModel"/> class.
+        /// </summary>
+        public MainViewModel()
+        {
+        }
+
+        private LibVLC _libVLC;
+
+        /// <summary>
+        /// Gets the <see cref="LibVLCSharp.Shared.LibVLC"/> instance.
+        /// </summary>
+        public LibVLC LibVLC
+        {
+            get => _libVLC;
+            private set => Set(nameof(LibVLC), ref _libVLC, value);
+        }
+
+        private MediaPlayer _mediaPlayer;
+        /// <summary>
+        /// Gets the <see cref="LibVLCSharp.Shared.MediaPlayer"/> instance.
+        /// </summary>
+        public MediaPlayer MediaPlayer
+        {
+            get => _mediaPlayer;
+            private set => Set(nameof(MediaPlayer), ref _mediaPlayer, value);
+        }
+
+        /// <summary>
+        /// Initialize LibVLC and playback when page appears
+        /// </summary>
+        public void OnAppearing()
+        {
+            Core.Initialize();
+
+            LibVLC = new LibVLC(enableDebugLogs: true);
+
+            var media = new Media(LibVLC, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
+
+            MediaPlayer = new MediaPlayer(media) { EnableHardwareDecoding = true };
+            media.Dispose();
+            MediaPlayer.Play();
+        }
+
+        internal void OnDisappearing()
+        {
+            MediaPlayer.Dispose();
+            LibVLC.Dispose();
+        }
+
+        private void Set<T>(string propertyName, ref T field, T value)
+        {
+            if (field == null && value != null || field != null && !field.Equals(value))
+            {
+                field = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+}
