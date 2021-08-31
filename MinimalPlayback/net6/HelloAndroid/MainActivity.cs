3fddce618aac0f1206be78f4b597774aadd1558e
mfkl <me@martinfinkel.com>
2021-08-31 16:41:32 +0700

    early net6 android sample
    not built by CI yet as its still on net5

---
+++ MinimalPlayback/net6/HelloAndroid/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using LibVLCSharp.Shared;
+using VideoView = LibVLCSharp.Platforms.Android.VideoView;
+
+namespace HelloAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        VideoView _videoView;
+        LibVLC _libVLC;
+        MediaPlayer _mediaPlayer;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            Core.Initialize();
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC)
+            {
+                EnableHardwareDecoding = true
+            };
+
+            _videoView = new VideoView(this) { MediaPlayer = _mediaPlayer };
+            AddContentView(_videoView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent));
+            using var media = new Media(_libVLC, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
+            _videoView.MediaPlayer.Play(media);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+            _videoView.MediaPlayer.Stop();
+            _videoView.Dispose();
+        }
+    }
+}
