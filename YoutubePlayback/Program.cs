db20cafa11d273dbbbdbc7a22b338b754fb6f0de
Martin Finkel <me@martinfinkel.com>
2021-11-29 14:14:59 +0700

    add youtube sample

---
+++ YoutubePlayback/Program.cs
@@ -0,0 +1,24 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YoutubePlayback
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Core.Initialize();
+
+            var libvlc = new LibVLC(enableDebugLogs: true);
+            var mediaplayer = new MediaPlayer(libvlc);
+
+            var media = new Media(libvlc, new Uri("https://www.youtube.com/watch?v=aqz-KE-bpKQ"));
+            await media.Parse(MediaParseOptions.ParseNetwork);
+            mediaplayer.Play(media.SubItems.First());
+
+            Console.ReadKey();
+        }
+    }
+}
